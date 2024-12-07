fbc3ae26c2c136646c4f4a6f1c1b46e618ae98b9
gabri <gabri@MARCCIELO>
2024-12-06 23:46:38 -0300

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: dotnet BlipTake.dll
--- obj/Debug/net8.0/BlipTake.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlipTake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlipTake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3665e7ab16f04bb64cb5f5e0a15663cdac7517")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlipTake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlipTake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
